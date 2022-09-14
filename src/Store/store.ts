66c6dfefa42d07ba61ad6af67e7ce9e72caeaa01
AleksTimofeev <timoha144a@gmail.com>
2022-09-14 15:17:34 +0300

    refactor: add AppThunkType

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,7 +1,11 @@
 import {AnyAction, applyMiddleware, compose, createStore} from "redux";
 import {rootReducer} from "./rootReducer";
 import {useDispatch} from "react-redux";
-import thunkMiddleware, {ThunkDispatch} from "redux-thunk";
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {AppActionsType} from "./appReducer";
+import {AuthActionsType} from "./authReducer";
+import {TaskActionsType} from "./taskReducer";
+import {TodolistsActionsType} from "./todolistsReducer";
 
 declare global {
   interface Window {
@@ -14,6 +18,10 @@ export type AppStateType = ReturnType<typeof rootReducer>
 
 export const useAppDispatch = () => useDispatch<ThunkDispatch<AppStateType,unknown,AnyAction> & AppDispatch>()
 export type AppDispatch = typeof store.dispatch
+
+export type ActionsType = AppActionsType | AuthActionsType | TaskActionsType | TodolistsActionsType
+
+export type AppThunkType = ThunkAction<void, AppStateType, unknown, ActionsType>
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
 
